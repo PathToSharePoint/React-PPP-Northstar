0e55a3fabd095a7f033d6a1d3a7c18d75f195bec
Christophe Humbert <Christophe@UserManagedSolutions.com>
2021-08-02 20:39:29 -0700

    create sppkg

---
+++ sharepoint/solution/debug/ClientSideAssets/ReactPPPNorthstarWebPartStrings_en-us_934ba68e523200142b0634b15b1b01d4.js
@@ -0,0 +1,8 @@
+define([], function() {
+  return {
+    "PropertyPaneDescription": "PPP demo with Fluent UI Northstar",
+    "BasicGroupName": "Built-in Controls",
+    "NorthstarGroupName": "Fluent UI Northstar Controls",
+    "DescriptionFieldLabel": "Description Field"
+  }
+});
